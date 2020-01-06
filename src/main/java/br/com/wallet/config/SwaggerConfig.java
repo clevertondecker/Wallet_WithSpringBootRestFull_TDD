67d9f95ce9d869a4e9c57a9bf466419ad3765d70
Cleverton.decker <Cleverton_decker@Hotmail.com>
2020-01-05 23:10:42 -0200

    Adicionando o Swagger

--- src/main/java/br/com/wallet/config/SwaggerConfig.java
+++ src/main/java/br/com/wallet/config/SwaggerConfig.java
@@ -34,7 +34,7 @@ public class SwaggerConfig {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.wallet.controller"))
+				.apis(RequestHandlerSelectors.basePackage("br.com.wallet.controller"))
 				.paths(PathSelectors.any()).build()
 				.apiInfo(apiInfo());
 	}
